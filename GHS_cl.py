dc1adf568906631378a37bebf638223aa96ee1db
Caylin <caiyunzhao_2022@163.com>
2026-03-20 12:26:32 +0800

    Add docstrings to classification functions
    Updated classification functions to include docstrings explaining toxicity thresholds and categories.

--- GHS_cl.py
+++ GHS_cl.py
@@ -2,13 +2,23 @@ import math
 
 
 def lc50_cl(values):
+    """
+    Classifies LC50 values into categories based on toxicity thresholds.
+    Thresholds are in log10 scale (log10(mg/L)).
+    
+    Categories:
+    1: High Toxicity (<= 1 mg/L)
+    2: Moderate Toxicity (1-10 mg/L)
+    3: Low Toxicity (10-100 mg/L)
+    0: Non-toxic / Out of range (> 100 mg/L)
+    """
     labels_cl = []
     for i in range(len(values)):
-        if values[i] <= math.log(1, 10): # 0
+        if values[i] <= math.log(1, 10): # 1 mg/L -> log10(1) = 0
             labels_cl.append(1)
-        elif values[i] > math.log(1, 10) and values[i] <= math.log(10, 10): # 1
+        elif values[i] > math.log(1, 10) and values[i] <= math.log(10, 10): # 10 mg/L
             labels_cl.append(2)
-        elif values[i] > math.log(10, 10) and values[i] <= math.log(100, 10): # 2
+        elif values[i] > math.log(10, 10) and values[i] <= math.log(100, 10): # 100 mg/L
             labels_cl.append(3)
         else:
             labels_cl.append(0)
@@ -16,17 +26,21 @@ def lc50_cl(values):
 
 
 def oral_ld50_cl(values):
+    """
+    GHS Classification for Oral LD50 (log10(mg/kg)).
+    Levels 1-5 correspond to increasing severity defined by the GHS system.
+    """
     labels_cl = []
     for i in range(len(values)):
-        if values[i] <= math.log(5, 10): # 0.7
+        if values[i] <= math.log(5, 10): # Level 1: <= 5 mg/kg
             labels_cl.append(1)
-        elif values[i]>math.log(5, 10) and values[i]<=math.log(50, 10): # 1.7
+        elif values[i]>math.log(5, 10) and values[i]<=math.log(50, 10): # Level 2
             labels_cl.append(2)
-        elif values[i]>math.log(50, 10) and values[i]<=math.log(300, 10): # 2.5
+        elif values[i]>math.log(50, 10) and values[i]<=math.log(300, 10): # Level 3
             labels_cl.append(3)
-        elif values[i]>math.log(300, 10) and values[i]<=math.log(2000, 10): # 3.3
+        elif values[i]>math.log(300, 10) and values[i]<=math.log(2000, 10): # Level 4
             labels_cl.append(4)
-        elif values[i] >math.log(2000, 10) and values[i]<=math.log(5000, 10): # 3.7
+        elif values[i] >math.log(2000, 10) and values[i]<=math.log(5000, 10): # Level 5
             labels_cl.append(5)
         else:
             labels_cl.append(0)
@@ -74,12 +88,4 @@ def simple_cl(values, label_name):
                 labels_cl.append('Non')
 
     return labels_cl
-
-
-
-
-
-
-
-
 
